fa4a2e87099710468111183d31de953d7074b0ac
RiansFonseca <20221TADSSAJ0002@ifba.edu.br>
2024-05-21 18:07:29 -0300

    First Commit

---
+++ src/test/java/gerenciamentoDeEnderecos/demoapigerenciaenderecos/DemoApiGerenciaEnderecosApplicationTests.java
@@ -0,0 +1,13 @@
+package gerenciamentoDeEnderecos.demoapigerenciaenderecos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApiGerenciaEnderecosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
